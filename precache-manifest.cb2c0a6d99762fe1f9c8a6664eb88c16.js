777bbc5a63ce3f23aba157811fa9596b9eac61b0
iamnahid <nrk.n9@yahoo.com>
2020-04-01 15:34:40 +0600

    Updates

---
+++ precache-manifest.cb2c0a6d99762fe1f9c8a6664eb88c16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b58f6ca58167f1b2b3750144bfc6fc9",
+    "url": "/business_SPA/index.html"
+  },
+  {
+    "revision": "207c90c939930505eb3f",
+    "url": "/business_SPA/static/css/main.29d40f25.chunk.css"
+  },
+  {
+    "revision": "dc83c0af2de19a67936d",
+    "url": "/business_SPA/static/js/2.51d6d3ec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/business_SPA/static/js/2.51d6d3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207c90c939930505eb3f",
+    "url": "/business_SPA/static/js/main.60ceeafd.chunk.js"
+  },
+  {
+    "revision": "fed90120a43cc5d3e905",
+    "url": "/business_SPA/static/js/runtime-main.d9fcac53.js"
+  },
+  {
+    "revision": "33d4f984655e36d8c89b0762ca21b30d",
+    "url": "/business_SPA/static/media/i1.33d4f984.jpg"
+  },
+  {
+    "revision": "b0074523c07e6601765fe28eb95abc94",
+    "url": "/business_SPA/static/media/man.b0074523.png"
+  },
+  {
+    "revision": "a4bbae4d4ee9bb571bce29f5328bf76e",
+    "url": "/business_SPA/static/media/php.a4bbae4d.jpg"
+  }
+]);
